2a329f4482de18f3848003e83bfa1af9dae2de07
Joao Oliveira <jhcardosodeoliveira@gmail.com>
2025-11-20 10:33:27 +0000

    Add project files.

---
+++ fs-2025-assessment-1-73599/Models/Station.cs
@@ -0,0 +1,31 @@
+﻿namespace fs_2025_assessment_1_73599.Models
+{
+
+	public class StationList
+	{
+		public Station[] ListOfStation { get; set; }
+	}
+
+	public class Station
+	{
+		public int number { get; set; }
+		public string contract_name { get; set; }
+		public string name { get; set; }
+		public string address { get; set; }
+		public Position position { get; set; }
+		public bool banking { get; set; }
+		public bool bonus { get; set; }
+		public int bike_stands { get; set; }
+		public int available_bike_stands { get; set; }
+		public int available_bikes { get; set; }
+		public string status { get; set; }
+		public long last_update { get; set; }
+	}
+
+	public class Position
+	{
+		public float lat { get; set; }
+		public float lng { get; set; }
+	}
+
+}
